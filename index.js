46d5b65a370bbbf5a0f45b93566979013921ff94
Aryankashyap266 <99300501+Aryankashyap266@users.noreply.github.com>
2023-02-15 13:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var randomNumber1 = Math.floor((Math.random()*6)+1);
 
 var randomDiceImage = "dice"+ randomNumber1+".png";
 
-var randomImageSource = "/"+randomDiceImage;
+var randomImageSource = "images/"+randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src" ,randomImageSource);
 
-//dice 2
+//dice
 
-document.querySelectorAll("img")[1].setAttribute("src" ,"/dice"+  Math.floor((Math.random()*6)+1)+".png");
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+var randomImageSource2 = "images/dice"+ randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src" ,randomImageSource2);
 
 if(randomNumber1>randomNumber2)
   {
